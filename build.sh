36446c8ea85f05a9fce51aeafd0f4b601426f682
sam bacha <sam@manifoldfinance.com>
2024-03-23 11:21:00 -0700

    build(mkdocs): use public instead of site dir

--- build.sh
+++ build.sh
@@ -16,19 +16,19 @@ source docs-venv/bin/activate
 pip3 install -r requirements.txt
 pip3 install git+https://${GH_TOKEN}@github.com/squidfunk/mkdocs-material-insiders.git
 
-mkdir -p site/.well-known
-mkdocs build --clean --strict --site-dir site/
-touch site/.nojekyll
-cp platform-docs.xml site/.well-known/platform-docs.xml
-cp commit-ts.txt site/.well-known/commit-ts.txt
+mkdir -p public/.well-known
+mkdocs build --clean --strict --site-dir public/
+touch public/.nojekyll
+cp platform-docs.xml public/.well-known/platform-docs.xml
+cp commit-ts.txt public/.well-known/commit-ts.txt
 
 header "Next Steps\n"
 echo "Build Artifact is available at: site/build_id.txt"
 
 
-date "+%Y-%m-%dT%H:%M:%S%z" > site/build_id.txt
+date "+%Y-%m-%dT%H:%M:%S%z" > public/build_id.txt
 
-TZ=UTC git show --quiet --date="format-local:%Y.%-m.%-d" --format="nightly-%cd" >site/.well-known/nightly-release.txt
+TZ=UTC git show --quiet --date="format-local:%Y.%-m.%-d" --format="nightly-%cd" > public/.well-known/nightly-release.txt
 sleep 1
 exit 0
 
