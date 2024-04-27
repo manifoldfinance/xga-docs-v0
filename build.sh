c5f14d55e3030296ceac61929261e22d5812eea3
sam bacha <sam@manifoldfinance.com>
2024-04-26 22:44:15 -0700

    fix(vercel): git env)

--- build.sh
+++ build.sh
@@ -24,7 +24,8 @@ mkdir -p site/.well-known
 mkdocs build --clean  --site-dir site/
 date "+%Y-%m-%dT%H:%M:%S%z" > site/build_id.txt
 
-TZ=UTC git show --quiet --date="format-local:%Y.%-m.%-d" --format="nightly-%cd" > site/.well-known/nightly-release.txt
+#TZ=UTC git show --quiet --date="format-local:%Y.%-m.%-d" --format="nightly-%cd" > site/.well-known/nightly-release.txt
+#cp nightly-release.txt site/.well-known/nightly-release.txt
 
 mike deploy --push --update-aliases 0.1.2 latest
 mike deploy v0.1.1
@@ -34,7 +35,7 @@ touch site/.nojekyll
 cp platform-docs.xml site/.well-known/platform-docs.xml
 cp commit-ts.txt site/.well-known/commit-ts.txt
 cp sitemap.xml site/sitemap.xml
-cp nightly-release.txt site/.well-known/nightly-release.txt
+
 
 header "Next Steps\n"
 echo "Build Artifact is available at: site/build_id.txt"
