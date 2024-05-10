6d9e1921ac1f6ff28ba2fcd6d0ea295bbd11e757
sam bacha <sam@manifoldfinance.com>
2024-05-09 18:34:23 -0700

    a4c9bcb @ 1.5.3

--- 0.2.1/javascripts/shourtcuts.js
+++
@@ -1,9 +0,0 @@
-keyboard$.subscribe(function (key) {
-  //  if (key.mode === "global" && key.type === "x") {
-  /* Add custom keyboard handler here */
-  if (key.mode === 'global' && key.key === 'k' && key.ctrl && key.meta) {
-    // Handle Command + K
-    console.log('Command + K was pressed');
-    key.claim();
-  }
-});
