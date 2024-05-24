a748dce966e009b75e9a9652666d9541f86d7519
sam bacha <sam@manifoldfinance.com>
2024-05-23 21:33:57 -0700

    update

--- build.sh
+++ build.sh
@@ -17,7 +17,6 @@ python3 -m venv docs-venv
 source docs-venv/bin/activate
 pip3 install -r requirements.txt
 pip3 install git+https://${GH_TOKEN}@github.com/squidfunk/mkdocs-material-insiders.git
-npm install
 
 
 mkdir -p site/.well-known
